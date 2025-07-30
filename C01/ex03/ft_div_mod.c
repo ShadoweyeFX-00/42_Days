d498c55d9d16103de5e4ba4e674f6db822db5fdc
Mouad Makhkhas <momakhkh@c2r8p4.1337.ma>
2025-07-30 06:13:57 +0100

    done

---
+++ C01/ex03/ft_div_mod.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_div_mod.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: momakhkh <momakhkh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/20 20:13:47 by momakhkh          #+#    #+#             */
+/*   Updated: 2025/07/20 22:22:57 by momakhkh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+	*div = a / b;
+	*mod = a % b;
+}
+/*
+int	main(void)
+{
+	int	a;
+	int	b;
+	int	div;
+	int	mod;
+
+	a = 31;
+	b = 17;
+	ft_div_mod(a, b, &div, &mod);
+	printf("A = %d  B = %d ", div, mod);
+}*/
