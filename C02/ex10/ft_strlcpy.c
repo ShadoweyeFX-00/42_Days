eaec12ef9591884982f1a5d32f0893cf0c51c0ab
Mouad Makhkhas <momakhkh@c3r9p13.1337.ma>
2025-08-07 17:33:55 +0100

    ss

--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: momakhkh <momakhkh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 04:44:20 by momakhkh          #+#    #+#             */
-/*   Updated: 2025/07/28 05:17:25 by momakhkh         ###   ########.fr       */
+/*   Updated: 2025/08/07 07:58:27 by momakhkh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 int main()
 {
 	char dest[] = "";
-	char src[] = "plambier";
+	char src[] = "SSWThresh";
 
 	printf("%i\n", ft_strlcpy(dest, src, 2));
 	printf("%s", dest);
